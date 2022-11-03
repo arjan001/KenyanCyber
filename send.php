55686aed583ee142aaa5c77e93c3a62aa001f5bf
snippet-softwares <snippetsoftwaresolutions@gmail.com>
2022-11-03 22:58:06 +0300

    Added email and SMTP server

--- send.php
+++ send.php
@@ -1,16 +1,16 @@
 <?php
-	$data = "Name: ".$_POST['name']."<br /><br />Email: ".$_POST['email']."<br /><br />Message<br />".$_POST['message'];
+	$data = "Name: ".$_POST['name']."<br /><br />Email: ".$_POST['email']."<br /><br />Message: ".$_POST['message'];
 	require 'phpmailer/PHPMailerAutoload.php';
 	$mail= new PHPMailer;
 	$mail->SMTPDebug = 1;
 	$mail->SMTPSecure = 'ssl';
-	$mail->Host = 'smtp.gmail.com';
+	$mail->Host = 'mail.smartcityplots.co.ke';
 	$mail->Port = 465;
 	$mail->isSMTP();
 	$mail->SMTPAuth=true;
-	$mail->Username='emmanuelmuema2@gmail.com';
-	$mail->Password='XXXXXX';
-	$mail->setFrom('emmanuelmuema2@gmail.com','Website Enquiry From KenyanCyber');
+	$mail->Username='test@smartcityplots.co.ke';
+	$mail->Password='^Q&(;rQfBGEF';
+	$mail->setFrom('test@smartcityplots.co.ke','Website Enquiry From KenyanCyber');
 	$mail->addAddress('emmanuelmuema52@gmail.com');
 	$mail->isHTML(true);
 	$mail->Subject=$_POST['subject'];
