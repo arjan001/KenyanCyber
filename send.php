19ccdbf514c971994e394c35fd1cd1f0e00433bd
snippet-softwares <snippetsoftwaresolutions@gmail.com>
2022-11-03 23:04:36 +0300

    Removed unwanted debug logs in send.php

--- send.php
+++ send.php
@@ -2,7 +2,7 @@
 	$data = "Name: ".$_POST['name']."<br /><br />Email: ".$_POST['email']."<br /><br />Message: ".$_POST['message'];
 	require 'phpmailer/PHPMailerAutoload.php';
 	$mail= new PHPMailer;
-	$mail->SMTPDebug = 1;
+//	$mail->SMTPDebug = 1;
 	$mail->SMTPSecure = 'ssl';
 	$mail->Host = 'mail.smartcityplots.co.ke';
 	$mail->Port = 465;
@@ -11,7 +11,8 @@
 	$mail->Username='test@smartcityplots.co.ke';
 	$mail->Password='^Q&(;rQfBGEF';
 	$mail->setFrom('test@smartcityplots.co.ke','Website Enquiry From KenyanCyber');
-	$mail->addAddress('emmanuelmuema52@gmail.com');
+    $mail->addAddress('abedimuange@gmail.com');
+    $mail->addAddress('emmanuelmuema52@gmail.com');
 	$mail->isHTML(true);
 	$mail->Subject=$_POST['subject'];
 	$mail->Body=$data;
